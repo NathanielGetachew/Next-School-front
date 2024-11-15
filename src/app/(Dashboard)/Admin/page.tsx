c4a217fc2ff3ed35c461420e53828a51bf8fa5f3
Nathaniel Getachew <nathigechmaranata@gmail.com>
2024-11-15 12:49:05 +0300

    adding calendar and event section

--- src/app/(Dashboard)/Admin/page.tsx
+++ src/app/(Dashboard)/Admin/page.tsx
@@ -1,3 +1,4 @@
+import Announcements from "@/components/Announcements"
 import AttendanceCharts from "@/components/AttendanceCharts"
 import CountCharts from "@/components/CountCharts"
 import EventCalendar from "@/components/EventCalendar"
@@ -35,7 +36,8 @@ const Admin = () => {
       </div>
       {/* right */}
       <div className=" w-full lg:w-1/3 flex flex-col gap-8">
-      <EventCalendar/>  
+      <EventCalendar/>
+      <Announcements/>
       </div>
     </div>
   )
--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -1,17 +1,57 @@
 "use client"
 
+import Image from "next/image"
 import { useState } from "react"
 import Calendar from "react-calendar"
 import "react-calendar/dist/Calendar.css"
 
 type valuePiece = Date | null
 type value = valuePiece | [valuePiece, valuePiece]
+
+// Temporary
+const events =[
+  {
+    id:1,
+    title:"Lorem ipsum dollar",
+    time: "12:00 PM - 2:00 PM",
+    description:"Lorem Ipsum dollar that are actually coming from america"
+  },
+  {
+    id:1,
+    title:"Lorem ipsum dollar",
+    time: "12:00 PM - 2:00 PM",
+    description:"Lorem Ipsum dollar that are actually coming from america "
+  },
+  {
+    id:1,
+    title:"Lorem ipsum dollar",
+    time: "12:00 PM - 2:00 PM",
+    description:"Lorem Ipsum dollar that are actually coming from america "
+  },
+]
+
+
 const EventCalendar = () => {
  const [value, onChange] = useState<value>(new Date())    
 
   return (
     <div className="bg-white p-4 rounded-md">
        <Calendar onChange={onChange} value={value}  /> 
+       <div className="flex items-center justify-between">
+        <h1 className="text-xl font-semibold my-4">Events</h1>
+        <Image src="/moreDark.png" alt="" width={20} height={20}/>
+       </div>
+       <div className="flex flex-col gap4">
+        {events.map(event =>(
+          <div className="p-5 rounded-md border-2 border-gray-100 border-t-4 odd:border-t-lamaSky even:border-t-lamaPurple" key={event.id}>
+          <div className="flex items-center justify-between">
+            <h1 className="font-semibold text-gray-600 text-xs">{event.title}</h1>
+            <span className="text-gray-300 text-xs">{event.time}</span>
+          </div>
+          <p className="mt-2 text-gray-400 text-sm">{event.description}</p>
+          </div>
+        ))}
+       </div>
     </div>
   )
 }
